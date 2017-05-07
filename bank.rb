541268e858314a59ca07451eb71ce5a693b38a20
Carl Hörberg <carl.hoerberg@gmail.com>
2017-05-07 19:16:55 -0400

    login and fetch balance

--- bank.rb
+++ bank.rb
@@ -1,11 +1,20 @@
 require "sinatra/base"
+require "excon"
+require "json"
 
 class BankController < Sinatra::Base
   get "/" do
     haml :login
   end
 
-  get "/:account" do
+  post "/" do
+    redirect "/#{params[:account]}"
+  end
+
+  get "/:account" do |account|
+    #resp = Excon.get "https://localhost:8080/balance/#{account}"
+    #data = JSON.parse resp.body
+    #@balance = data["balance"]
     haml :account
   end
 end
